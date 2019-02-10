cd0884953d50dc2d6bce0d814adf6e5071bddabb
Ngonidzashe <dchibamu@gmail.com>
2019-02-10 18:11:53 +0200

    Added package structure and unit tests class

---
+++ src/main/java/com/gojek/service/ParkingLot.java
@@ -0,0 +1,4 @@
+package com.gojek.service;
+
+public class ParkingLot {
+}
---
+++ src/test/java/com/gojek/service/ParkingLotTest.java
@@ -0,0 +1,8 @@
+package com.gojek.service;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("Unit tests assertions for ParkingLot service ")
+public class ParkingLotTest {
+    
+}
