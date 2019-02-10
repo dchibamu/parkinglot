63091b4a72bfc0d1ae5346e2e7ded082de64f74c
Ngonidzashe <dchibamu@gmail.com>
2019-02-10 19:59:56 +0200

    Added unit tests to check attempt to create parking lot with invalid size

--- src/test/java/com/gojek/service/ParkingLotTest.java
+++ src/test/java/com/gojek/service/ParkingLotTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 @DisplayName("Unit tests assertions for ParkingLot service ")
 class ParkingLotTest {
 
     private static final String EXPECTED_OUTPUT_FROM_CREATE_PARKING_LOT="Created a parking lot with %s slots";
+    private static final String EXPECTED_OUT_WHEN_PARKINGLOT_IS_FULL="Sorry, parking lot is full";
 
     @Test
     @DisplayName("Should be able to create a parking lot with 10 spaces")
@@ -18,5 +20,13 @@ class ParkingLotTest {
         int parkingLotCapacity = 10;
         ParkingLot parkingLot = new ParkingLot();
         assertThat(parkingLot.createParkingLot(parkingLotCapacity), is(equalTo(String.format(EXPECTED_OUTPUT_FROM_CREATE_PARKING_LOT, parkingLotCapacity))));
+    }
+
+    @Test
+    @DisplayName("Parking lot size should only be of size 1 or more")
+    void shouldReturnNothingIfParkingLotSizeIsLessThanOne(){
+        int parkingLotCapacity = 0;
+        ParkingLot parkingLot = new ParkingLot();
+        assertThat(parkingLot.createParkingLot(parkingLotCapacity), is(not(equalTo(String.format(EXPECTED_OUTPUT_FROM_CREATE_PARKING_LOT, parkingLotCapacity)))));
     }
 }
