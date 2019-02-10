fa6dee5ce28bcf0887fdfd110137e5c6aa52be7f
Ngonidzashe <dchibamu@gmail.com>
2019-02-10 18:38:15 +0200

    Added unit test for creating parking lot

--- src/main/java/com/gojek/service/ParkingLot.java
+++ src/main/java/com/gojek/service/ParkingLot.java
@@ -1,4 +1,8 @@
 package com.gojek.service;
 
 public class ParkingLot {
+
+    public String createParkingLot(int size){
+        return "Created a parking lot with 10 slots";
+    }
 }
--- src/test/java/com/gojek/service/ParkingLotTest.java
+++ src/test/java/com/gojek/service/ParkingLotTest.java
@@ -1,10 +1,21 @@
 package com.gojek.service;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 @DisplayName("Unit tests assertions for ParkingLot service ")
-public class ParkingLotTest {
+class ParkingLotTest {
 
-
-
+    private static final String EXPECTED_OUTPUT_FROM_CREATE_PARKING_LOT="Created a parking lot with %s slots";
+    @Test
+    @DisplayName("Should be able to create a parking lot with 10 spaces")
+    void shouldCreateParkingLotOfSizeTen(){
+        int parkingLotCapacity = 10;
+        ParkingLot parkingLot = new ParkingLot();
+        assertThat(parkingLot.createParkingLot(parkingLotCapacity), is(equalTo(String.format(EXPECTED_OUTPUT_FROM_CREATE_PARKING_LOT, parkingLotCapacity))));
+    }
 }
