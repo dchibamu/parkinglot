26ba4e0757808748abeeea87c2a98b5e78c16759
Ngonidzashe <dchibamu@gmail.com>
2019-02-11 10:27:44 +0200

    Added main class to drive the different parking lots

---
+++ src/main/java/com/gojek/ParkingLotMain.java
@@ -0,0 +1,41 @@
+package com.gojek;
+
+import com.gojek.service.ParkingLot;
+import java.nio.file.Path;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class ParkingLotMain {
+
+    public static void main(String[] args) {
+        int numberOfArgs = args.length;
+        switch(numberOfArgs){
+            case 0:
+                runInteractiveParkingLot();
+                break;
+            case 1:
+                Path commandsFile = Paths.get(args[0]);
+                runFileParkingLot(commandsFile);
+                break;
+        }
+    }
+
+    private static void runInteractiveParkingLot(){
+        do {
+            //TODO: use a scanner to read user input from console.
+        }while(true);
+    }
+
+    private static void runFileParkingLot(Path filePath){
+        ParkingLot parkingLot = new ParkingLot();
+        try(Stream<String> commandStream = Files.lines(filePath)){
+            //TODO: call decision making method from ParkingLot
+        }catch (IOException e){
+            //correct approach is to log the error, however, for this challenge am not sure what effect that logging has on
+            //the automated testing platform.
+        }
+    }
+}
